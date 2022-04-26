d2469611fc79a37618306b124b11f591b09c6b00
Egor Rumyantsev <RumyantsevGT@ya.ru>
2022-04-26 23:37:23 +0300

    Update script.js

--- spektr/script.js
+++ spektr/script.js
@@ -64,25 +64,42 @@ var colorPicker = new iro.ColorPicker(".colorPicker", {
   width: 280,
   color: "rgb(255, 0, 0)",
   borderWidth: 1,
-  borderColor: "#fff" });
+  borderColor: "#fff" ,
+  layout: [
+    {
+      component: iro.ui.Box,
+    },
+    {
+      component: iro.ui.Slider,
+      options: {
+        id: 'hue-slider',
+        sliderType: 'hue'
+      }
+    }
+  ]
+  });
 
 
+
+  /*
 var values = document.getElementById("values");
 var hexInput = document.getElementById("hexInput");
+*/
 
-// https://iro.js.org/guide.html#color-picker-events
-colorPicker.on(["color:init", "color:change"], function (color) {
+
+  // https://iro.js.org/guide.html#color-picker-events
+  colorPicker.on(["color:init", "color:change"], function (color) {
   // Show the current color in different formats
   // Using the selected color: https://iro.js.org/guide.html#selected-color-api
   values.innerHTML = [
   "hex: " + color.hexString,
   "rgb: " + color.rgbString,
   "hsl: " + color.hslString].
-  join("<br>");
+            join("<br>");
 
   hexInput.value = color.hexString;
-});
+ });
 
-hexInput.addEventListener('change', function () {
+  hexInput.addEventListener('change', function () {
   colorPicker.color.hexString = this.value;
-});
+ });
